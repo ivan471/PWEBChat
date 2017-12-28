ec093ce441ed9edc5bc07e4f81f18ee1ddc2726c
Ivan Darmawan <32323720+ivan471@users.noreply.github.com>
2017-12-29 03:21:06 +0800

    Hampir selesai

--- app/src/main/java/com/example/ivan/pwebchat/MainActivity.java
+++ app/src/main/java/com/example/ivan/pwebchat/MainActivity.java
@@ -6,6 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -56,7 +59,8 @@ public class MainActivity extends AppCompatActivity {
             public void onCancelled(DatabaseError error) {
             }
         });
-        // PENGAKTIFAN RecyclerView MENGGUNAKAN ChatListAdapter
+
+            // PENGAKTIFAN RecyclerView MENGGUNAKAN ChatListAdapter
         rvChats = (RecyclerView)findViewById(R.id.rvchats);
         rvChats.setHasFixedSize(true);
         rvChats.setLayoutManager(new LinearLayoutManager(this));
@@ -76,5 +80,27 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menuUser:
+                Intent intent = new Intent(MainActivity.this,
+                        UserListActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.menuLogout:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
